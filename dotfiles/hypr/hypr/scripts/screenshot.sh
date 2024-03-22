c7c8bf11aee73734ad6fd0180a734d8d99a28456
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-03-22 09:59:12 +0530

    Update version numbers and configurations

--- dotfiles/hypr/hypr/scripts/screenshot.sh
+++ dotfiles/hypr/hypr/scripts/screenshot.sh
@@ -10,10 +10,11 @@
 # ----------------------------------------------------- 
 
 DIR="$HOME/Pictures/screenshots/"
-NAME="screenshot_$(date +%d%m%Y_%H%M%S).png"
+
+NAME="screenshot_$(date +%Y-%m-%d_%H%M%S).png"
 
 option2="Selected area"
-option3="Fullscreen (delay 3 sec)"
+option3="Fullscreen"
 
 options="$option2\n$option3"
 
@@ -21,12 +22,16 @@ choice=$(echo -e "$options" | rofi -dmenu -replace -config ~/dotfiles/rofi/confi
 
 case $choice in
     $option2)
-        grim -g "$(slurp)" - | swappy -f -
-        notify-send "Screenshot created" "Mode: Selected area"
+        grim -g "$(slurp)" "$DIR$NAME"
+        xclip -selection clipboard -t image/png -i "$DIR$NAME"
+        swappy -f -
+        notify-send -t 250 "Screenshot created and copied to clipboard" "Mode: Selected area"
     ;;
     $option3)
-        sleep 3
-        grim - | swappy -f -
-        notify-send "Screenshot created" "Mode: Fullscreen"
+        sleep 0.75
+        grim "$DIR$NAME"
+        xclip -selection clipboard -t image/png -i "$DIR$NAME"
+        swappy -f -
+        notify-send -t 250 "Screenshot created and copied to clipboard" "Mode: Fullscreen"
     ;;
 esac
