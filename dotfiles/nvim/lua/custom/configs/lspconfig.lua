a5fecc4083db487d13570d03242f1e02c0e06aaf
Codesmith28 <Codesmith28@users.noreply.github.com>
2024-05-22 12:37:10 +0530

    make discord a startup app and add go support in nvim

--- dotfiles/nvim/lua/custom/configs/lspconfig.lua
+++ dotfiles/nvim/lua/custom/configs/lspconfig.lua
@@ -3,7 +3,10 @@ local on_attach = base.on_attach
 local capabilities = base.capabilities
 
 local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
 
+
+-- for clang
 lspconfig.clangd.setup {
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
@@ -12,6 +15,7 @@ lspconfig.clangd.setup {
   capabilities = capabilities,
 }
 
+-- for typescript
 lspconfig.tsserver.setup {
     on_attach = on_attach,
     capabilities = capabilities,
@@ -21,6 +25,25 @@ lspconfig.tsserver.setup {
         }
     }
 }
+
+-- for go
+lspconfig.gopls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = {"gopls"},
+    filetypes = {"go", "gomod", "gowork", "gotmpl"},
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            },
+        },
+    },
+}
+
 
 local servers = {"tailwindcss", "eslint"}
 
--- dotfiles/nvim/lua/custom/configs/null-ls.lua
+++ dotfiles/nvim/lua/custom/configs/null-ls.lua
@@ -6,7 +6,13 @@ local opts = {
         null_ls.builtins.formatting.clang_format,
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.golines,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.sqlformat,
     },
+    -- format on save:
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({
