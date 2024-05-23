d276961f72cfd36a98a8b235a1991189989cbea7
Codesmith28 <Codesmith28@users.noreply.github.com>
2024-05-23 19:13:29 +0530

    tmux setup script added

---
+++ setupScripts/setupTmux/setuptmux.sh
@@ -0,0 +1,18 @@
+cat <<"EOF"
+ _
+| |_ _ __ ___  _   ___  __
+| __| '_ ` _ \| | | \ \/ /
+| |_| | | | | | |_| |>  <
+ \__|_| |_| |_|\__,_/_/\_\
+
+
+EOF
+
+#! /bin/bash
+# These are the configurations of omerxx for tmux!
+echo "Thank you omerxx's for your tmux configurations!"
+
+# Installing tmux and tpm
+yay -S tmux tmux-plugin-manager --noconfirm
+
+echo "Copy from the dotfiles and then hit Ctrl+I"
