0d6ca683492b4de190d12670f3c86da653a5d841
Codesmith28 <Codesmith28@users.noreply.github.com>
2024-05-25 11:04:11 +0530

    refactor nvim config

--- dotfiles/nvim/lua/options.lua
+++ dotfiles/nvim/lua/options.lua
@@ -3,5 +3,22 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt = 'both' -- to enable cursorline!
 
+-- relative line numbering:
+o.relativenumber = true
+
+-- set tabs to have 4 spaces
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
+
+-- other utilites
+vim.g.copilot_assume_mapped = true
+
+-- shell and search settings
+o.shell = "zsh"
+o.ignorecase = true
+o.smartcase = true
+
+-- o.cursorlineopt ='both' -- to enable cursorline!
