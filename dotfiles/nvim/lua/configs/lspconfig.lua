7fb717dffacce024e8be3156d823d8f23d5b8d35
Codesmith28 <Codesmith28@users.noreply.github.com>
2024-05-28 10:40:12 +0530

    update dotfiles

--- dotfiles/nvim/lua/configs/lspconfig.lua
+++ dotfiles/nvim/lua/configs/lspconfig.lua
@@ -1,53 +1,55 @@
--- EXAMPLE
 local base = require("nvchad.configs.lspconfig")
 local on_attach = base.on_attach
-local on_init = base.on_init
 local capabilities = base.capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
+
 
 -- for clang
 lspconfig.clangd.setup {
-  on_attach = function(client, bufnr)
-    client.server_capabilities.signatureHelpProvider = false
-    on_attach(client, bufnr)
-  end,
-  capabilities = capabilities,
-}
-
--- for go language
-lspconfig.gopls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      },
-    },
-  },
-}
-
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
-local servers = { "html", "cssls", "tailwindcss", "eslint" }
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
+    on_attach = function(client, bufnr)
+        client.server_capabilities.signatureHelpProvider = false
+        on_attach(client, bufnr)
+    end,
     capabilities = capabilities,
-  }
+}
+
+-- for typescript
+lspconfig.tsserver.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    init_options = {
+        preferences = {
+            disableSuggestions = true,
+        }
+    }
+}
+
+-- for go
+lspconfig.gopls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            },
+        },
+    },
+}
+
+
+local servers = { "tailwindcss", "eslint" }
+
+for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
 end
