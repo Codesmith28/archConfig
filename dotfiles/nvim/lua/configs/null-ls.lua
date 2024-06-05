a0cb3183b3110f40c156feabcc385ab7a34975f9
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-06-05 10:49:12 +0530

    update dotfiles and nvim config

--- dotfiles/nvim/lua/configs/null-ls.lua
+++ dotfiles/nvim/lua/configs/null-ls.lua
@@ -1,5 +1,5 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 local opts = {
     sources = {
@@ -14,16 +14,16 @@ local opts = {
     },
     -- format on save:
     on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({
+        if client.supports_method "textDocument/formatting" then
+            vim.api.nvim_clear_autocmds {
                 group = augroup,
                 buffer = bufnr,
-            })
+            }
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = augroup,
                 buffer = bufnr,
                 callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
+                    vim.lsp.buf.format { bufnr = bufnr }
                 end,
             })
         end
