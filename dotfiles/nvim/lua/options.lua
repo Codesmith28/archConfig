bc5f6d60afcdb87d3068a9bc2ba43b5f8a2da073
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-07-29 18:25:43 +0530

    update nvim config

--- dotfiles/nvim/lua/options.lua
+++ dotfiles/nvim/lua/options.lua
@@ -43,3 +43,14 @@ vim.api.nvim_set_keymap("n", "<S-ScrollWheelDown>", "10zl", { noremap = true, si
 -- comments
 vim.api.nvim_set_keymap("n", "<C-/>", "gcc", { noremap = false })
 vim.api.nvim_set_keymap("v", "<C-/>", "gcc", { noremap = false })
+
+-- for neovide
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
+
+vim.g.neovide_floating_shadow = true
+vim.g.neovide_floating_z_height = 10
+vim.g.neovide_light_angle_degrees = 45
+vim.g.neovide_light_radius = 5
+
+vim.g.neovide_transparency = 0.8
