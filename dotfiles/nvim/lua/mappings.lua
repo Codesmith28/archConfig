4018a70d83084043370a1babda1b182d2d65f18e
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-11-14 08:09:25 +0530

    update kitty and nvim

--- dotfiles/nvim/lua/mappings.lua
+++ dotfiles/nvim/lua/mappings.lua
@@ -53,10 +53,6 @@ vim.opt.mouse = "a"
 map("n", "<S-ScrollWheelUp>", "10zh", { noremap = true, silent = true })
 map("n", "<S-ScrollWheelDown>", "10zl", { noremap = true, silent = true })
 
--- comments
-map("n", "<C-/>", "gcc", { noremap = false })
-map("v", "<C-/>", "gcc", { noremap = false })
-
 -- harpoon mappings
 map("n", "<leader>ah", "<cmd>lua require('harpoon.mark').add_file()<CR>", { desc = "Add mark" })
 map("n", "<leader>lh", "<cmd>Telescope harpoon marks<CR>", { desc = "Toggle mark telescope" })
