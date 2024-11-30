16383a098f401b99fbc48e513c8e4945396f6640
Codesmith28 <Codesmith28@users.noreply.github.com>
2024-11-30 10:59:24 +0530

    update yazi and setup scripts

--- setupScripts/setupSystemFonts/setup.sh
+++ setupScripts/setupSystemFonts/setup.sh
@@ -5,5 +5,8 @@ if [ $EUID -ne 0 ]; then
     exit 1
 fi
 
+# remove all the monospace fonts under /usr/share/fonts/noto && /usr/share/fonts/liberation/
+rm -rf /usr/share/fonts/noto/*Mono*
+rm -rf /usr/share/fonts/liberation/*Mono*
 
-cp ./conf.d/60-latin.conf ~/.config/fontconfig/conf.d/60-latin.conf
+cp ./conf.d/60-latin.conf ~/dotfiles/fontconfig/conf.d/60-latin.conf
