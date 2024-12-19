1fb06ebc4b742c81c3923a106af74b8ae1021ff0
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-12-19 19:43:39 +0530

    update gnome config to enable copy up and down

--- black_box/config_gnome/config.sh
+++ black_box/config_gnome/config.sh
@@ -4,5 +4,10 @@
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['']"
 
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Page_Up']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Page_Down']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Page_Up']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Page_Down']"
+
 # open new windows in the center:
 gsettings set org.gnome.mutter center-new-windows true
