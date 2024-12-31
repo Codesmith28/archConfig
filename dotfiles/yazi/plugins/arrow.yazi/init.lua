ba82731dedd4e5193589c9bd00cef6aaa0f5e7ca
Codesmith28 <Codesmith28@users.noreply.github.com>
2025-01-01 05:20:43 +0530

    clean dotfiles

--- dotfiles/yazi/plugins/arrow.yazi/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-	entry = function(_, args)
-		local current = cx.active.current
-		local new = (current.cursor + args[1]) % #current.files
-		ya.manager_emit("arrow", { new - current.cursor })
-	end,
-}
-
