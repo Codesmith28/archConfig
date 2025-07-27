0c75132a4fab6f678dd183d728e0eb70903fb5e8
Codesmith28 <sarthaksiddhpura@gmail.com>
2025-07-27 22:35:57 +0530

    update
    this includes the addition of specific setup scripts for Tessa,
    nvidia-setup and set-battery

--- ubuntu/dotfiles/nvim/lua/plugins/snippets.lua
+++ ubuntu/dotfiles/nvim/lua/plugins/snippets.lua
@@ -1,10 +1,12 @@
 return {
     "L3MON4D3/LuaSnip",
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
-        require("luasnip/loaders/from_vscode").lazy_load { paths = "~/.config/nvim/lua/snippets" }
-        require("luasnip").setup {
+        require("luasnip.loaders.from_snipmate").lazy_load()
+        require("luasnip/loaders/from_vscode").lazy_load({ paths = "~/.config/nvim/lua/snippets" })
+        require("luasnip").setup({
             updateevents = "TextChanged, TextChangedI",
-        }
+        })
     end,
 }
--- ubuntu/dotfiles/sync.sh
+++ ubuntu/dotfiles/sync.sh
@@ -4,32 +4,34 @@ DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Map of items to destinations
 declare -A SYMLINKS=(
-  [.bashrc]="$HOME"
-  [.profile]="$HOME"
-  [.tmux]="$HOME"
-  [.zshrc]="$HOME"
-  [kitty]="$HOME/.config"
-  [nvim]="$HOME/.config"
-  [starship]="$HOME/.config"
-  [television]="$HOME/.config"
-  [tmux]="$HOME/.config"
-  [yazi]="$HOME/.config"
+    [.bashrc]="$HOME"
+    [.profile]="$HOME"
+    [.tmux]="$HOME"
+    [.zshrc]="$HOME"
+    [kitty]="$HOME/.config"
+    [nvim]="$HOME/.config"
+    [starship]="$HOME/.config"
+    [television]="$HOME/.config"
+    [tmux]="$HOME/.config"
+    [yazi]="$HOME/.config"
+    [work]="$HOME/.config"
+    [scripts]="$HOME/.config"
 )
 
 link_item() {
-  local item="$1"
-  local dest="${SYMLINKS[$item]}"
-  local target="$dest/$item"
-  local source="$DOTFILES_DIR/$item"
+    local item="$1"
+    local dest="${SYMLINKS[$item]}"
+    local target="$dest/$item"
+    local source="$DOTFILES_DIR/$item"
 
-  [ -e "$target" ] || [ -L "$target" ] && rm -rf "$target"
-  mkdir -p "$dest"
-  ln -s "$source" "$target"
-  echo "Linked $source -> $target"
+    [ -e "$target" ] || [ -L "$target" ] && rm -rf "$target"
+    mkdir -p "$dest"
+    ln -s "$source" "$target"
+    echo "Linked $source -> $target"
 }
 
 for item in "${!SYMLINKS[@]}"; do
-  link_item "$item"
+    link_item "$item"
 done
 
 echo "All symlinks created."
