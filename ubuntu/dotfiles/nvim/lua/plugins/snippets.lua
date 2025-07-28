eba98ff46fc19d6419667a8e1fbc12a188e001e4
Codesmith28 <sarthaksiddhpura@gmail.com>
2025-07-28 14:17:34 +0530

    update nvim config

--- ubuntu/dotfiles/nvim/lua/plugins/snippets.lua
+++ ubuntu/dotfiles/nvim/lua/plugins/snippets.lua
@@ -1,12 +1,21 @@
 return {
     "L3MON4D3/LuaSnip",
-    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    version = "v2.*",
     dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
+        -- Load friendly-snippets
+        require("luasnip.loaders.from_vscode").lazy_load()
+        -- Load your custom snippets
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/lua/snippets" })
+        -- Load snipmate style snippets
         require("luasnip.loaders.from_snipmate").lazy_load()
-        require("luasnip/loaders/from_vscode").lazy_load({ paths = "~/.config/nvim/lua/snippets" })
+
         require("luasnip").setup({
-            updateevents = "TextChanged, TextChangedI",
+            updateevents = "TextChanged,TextChangedI",
+            -- Enable autotriggered snippets
+            enable_autosnippets = true,
+            -- Store snippet history for jumps
+            store_selection_keys = "<Tab>",
         })
     end,
 }
---
+++ ubuntu/dotfiles/nvim/test.cpp
@@ -0,0 +1,8 @@
+// Test file for C++ snippets
+#include <iostream>
+
+int main() {
+    // Try typing "for" and see if snippets work
+    
+    return 0;
+}
