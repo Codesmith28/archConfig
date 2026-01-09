7b189e8fec4e817e10fefc7a955568c99be7e1bc
Codesmith28 <sarthaksiddhpura@gmail.com>
2026-01-09 16:05:27 +0530

    add lazygit install and default to vim

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -72,8 +72,11 @@ sudo ln -sf /usr/bin/fdfind /usr/local/bin/fd
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# nvim
-sudo snap install nvim --classic
+# lazyvim
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 
 # ----------------------------------------------------
 # starship prompt
