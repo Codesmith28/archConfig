000d1a934f55bf4a669d59dda62689c69f5a8fc0
Codesmith28 <Codesmith28@users.noreply.github.com>
2026-06-14 20:15:35 +0530

    update snippets

--- mac/dotfiles/config/nvim/lua/plugins/theme.lua
+++ mac/dotfiles/config/nvim/lua/plugins/theme.lua
@@ -10,26 +10,6 @@ return {
                 show_end_of_buffer = true, -- hide ~ at end of buffer
                 term_colors = true, -- terminal colors matching theme
 
-                -- -- Adwaita Background
-                -- custom_highlights = function(colors)
-                --     return {
-                --         Normal = { bg = "#171717", fg = "#deddda" },
-                --         -- NormalFloat = { bg = "#171717", fg = "#deddda" },
-                --         -- FloatBorder = { bg = "#171717", fg = "#deddda" },
-                --
-                --         -- Selections
-                --         Visual = { bg = "#303030", fg = "#c0bfbc" },
-                --         VisualNOS = { bg = "#303030", fg = "#c0bfbc" },
-                --
-                --         -- Cursor line (optional but recommended)
-                --         CursorLine = { bg = "#1f1f1f" },
-                --
-                --         -- UI Elements
-                --         LineNr = { fg = "#5a5a5a" },
-                --         CursorLineNr = { fg = "#deddda" },
-                --     }
-                -- end
-
                 integrations = {
                     native_lsp = {
                         enabled = false,
@@ -45,6 +25,34 @@ return {
                 },
             })
         end,
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = {
+            transparent = true, -- Enable transparent background
+
+            styles = {
+                comments = { italic = false },
+            },
+
+            -- Optional: adjust specific highlight groups if you want even more transparency
+            on_highlights = function(hl, c)
+                hl.TelescopeNormal = {
+                    bg = c.none,
+                    fg = c.fg_dark,
+                }
+                hl.TelescopeBorder = {
+                    bg = c.none,
+                    fg = c.fg_dark,
+                }
+                hl.LspInlayHint = {
+                    -- bg = c.none, -- Removes the background entirely
+                    fg = c.dark5, -- Keeps the text muted
+                }
+            end,
+        },
     },
 
     {
